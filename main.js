ba6cfab4a7b8aeef78221546e10cc76483f62c58
Caleb Tseng <calebtseng07@gmail.com>
2023-03-18 04:52:27 -0700

    Create function to store and display new books

--- main.js
+++ main.js
@@ -5,13 +5,36 @@ let overlay = document.querySelector("#overlay");
 let bookContent = document.querySelector(".book-content");
 let addBook = document.querySelector("add-book");
 
-let titleInput = document.querySelector("#title");
-let authorInput = document.querySelector("#author");
-let pagesInput = document.querySelector("#pages");
-let readInput = document.querySelector("#read");
-let bookReadInput = document.querySelector("#bookRead");
+// let titleInput = document.querySelector("#title");
+// let authorInput = document.querySelector("#author");
+// let pagesInput = document.querySelector("#pages");
+// let readInput = document.querySelector("#read");
+// let bookReadInput = document.querySelector("#bookRead");
 
 let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").value;
+    let bookRead = document.querySelector("#bookRead").value;
+    let newBookInfo = new Book(title, author, pages, read);
+    myLibrary.push(newBookInfo)
+    console.log(myLibrary);
+}
+
+document.querySelector('#add-book').addEventListener("submit", function (event) {
+    event.preventDefault();
+    addBookToLibrary();
+})
 
 newBook.forEach(button => {
     button.addEventListener('click', () => {
@@ -54,34 +77,34 @@ function closeModal(modal) {
 
 // }
 
-class Book {
-    constructor(title, author, pages, read, bookRead) {
-        this.title = title
-            .trim()
-            .split(" ")
-            .map((word) => {
-                return word[0].toUpperCase() + word.substring(1).toLowerCase();
-            })
-            .join(" ");
-        this.author = author
-            .trim()
-            .split(" ")
-            .map((word) => {
-                return word[0].toUpperCase() + word.substring(1).toLowerCase();
-            })
-            .join(" ");
-        this.pages = pages;
-        this.read = read;
-        this.bookRead = bookRead;
-    }
-}
+// class Book {
+//     constructor(title, author, pages, read, bookRead) {
+//         this.title = title
+//             .trim()
+//             .split(" ")
+//             .map((word) => {
+//                 return word[0].toUpperCase() + word.substring(1).toLowerCase();
+//             })
+//             .join(" ");
+//         this.author = author
+//             .trim()
+//             .split(" ")
+//             .map((word) => {
+//                 return word[0].toUpperCase() + word.substring(1).toLowerCase();
+//             })
+//             .join(" ");
+//         this.pages = pages;
+//         this.read = read;
+//         this.bookRead = bookRead;
+//     }
+// }
 
 
 
 
 // function addBookToLibrary() {
 //     let newBook = new Book(
-//         titleInput.value,
+//     .value,
 //         authorInput.value,
 //         pagesInput.value,
 //         readInput.value,
